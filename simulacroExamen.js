e63810d7b3965aa9a3ef034daece1542a7e45bd2
adriangoll <maselchuecoleon@gmail.com>
2024-03-26 20:46:57 -0300

    Simulacro del examen del miercoles 27

---
+++ simulacroExamen.js
@@ -0,0 +1,28 @@
+// Crear una función que reciba como parámetro una nota y retorne una de 
+// estas 3 palabras: LIBRE, REGULAR o PROMOCIONADO, en dependencia de:
+
+// Nota es menor a 4 - LIBRE
+// Nota es menor a 7 y no es LIBRE - REGULAR 
+// Nota mayor que 6  - PROMOCIONADO.
+
+
+
+function condicionAlumno(nota){
+    
+    if(nota < 4){
+        return "LIBRE"
+    }
+    else if (nota < 7 & nota >=4){
+        return "REGULAR"
+    }
+    else if (nota > 6 & nota <=10){
+        return "PROMOCIONADO"
+    }
+    else{
+        return "REVISA EL DATO INGRESADO"
+    }
+}
+let nota = prompt("ingrese la nota");
+nota = parseInt(nota);
+condicion = condicionAlumno(nota)
+console.log(`Su nota fue de: ${nota}, y su condicion es: ${condicion}`)
